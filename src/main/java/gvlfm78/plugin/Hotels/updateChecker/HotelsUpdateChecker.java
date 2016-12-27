8b931668b62548124a7b3482f085924d0c88d4eb
kernitus <2789734+kernitus@users.noreply.github.com>
2016-12-27 14:51:05 +0100

    Made bukkit update checker perform a version check too

--- src/main/java/gvlfm78/plugin/Hotels/updateChecker/HotelsUpdateChecker.java
+++ src/main/java/gvlfm78/plugin/Hotels/updateChecker/HotelsUpdateChecker.java
@@ -31,15 +31,19 @@ public class HotelsUpdateChecker {
 			//Get messages from Spigot update checker
 			if(SUC.getResult().name().equalsIgnoreCase("UPDATE_AVAILABLE")){
 				//An update is available
-				updateMessages[0] = Mes.mesnopre("main.updateAvailable").replaceAll("%version%",SUC.getVersion());
-				updateMessages[1] = Mes.mesnopre("main.updateAvailableLink").replaceAll("%link%","https://www.spigotmc.org/resources/hotels.2047/updates/");
+				updateMessages[0] = Mes.mesnopre("main.updateAvailable").replaceAll("%version%", SUC.getVersion());
+				updateMessages[1] = Mes.mesnopre("main.updateAvailableLink").replaceAll("%link%", "https://www.spigotmc.org/resources/hotels.2047/updates/");
 			}
 		}
 		else{//Get messages from bukkit update checker
 			Updater updater = new Updater(plugin, 70177, pluginFile, Updater.UpdateType.NO_DOWNLOAD, false);
 			if(updater.getResult().equals(UpdateResult.UPDATE_AVAILABLE)){
-				updateMessages[0] = Mes.mesnopre("main.updateAvailable").replaceAll("%version%", updater.getLatestName().replaceAll("[A-Za-z\\s]", ""));
-				updateMessages[1] = Mes.mesnopre("main.updateAvailableLink").replaceAll("%link%", updater.getLatestFileLink());
+				//Updater knows local and remote versions are different, but not if it's an update
+				String remoteVersion = updater.getLatestName().replaceAll("[A-Za-z\\s]", "");
+				if(shouldUpdate(plugin.getDescription().getVersion(), remoteVersion)){
+					updateMessages[0] = Mes.mesnopre("main.updateAvailable").replaceAll("%version%", remoteVersion);
+					updateMessages[1] = Mes.mesnopre("main.updateAvailableLink").replaceAll("%link%", updater.getLatestFileLink());
+				}
 			}
 		}
 		return updateMessages;
@@ -66,5 +70,26 @@ public class HotelsUpdateChecker {
 		else{
 			sendUpdateMessages(plugin.getLogger());
 		}
+	}
+	public boolean shouldUpdate(String localVersion, String remoteVersion) {
+		return versionCompare(localVersion, remoteVersion) < 0;
+		//return !localVersion.equalsIgnoreCase(remoteVersion);
+	}
+	public Integer versionCompare(String oldVer, String newVer){
+		String[] vals1 = oldVer.split("\\.");
+		String[] vals2 = newVer.split("\\.");
+		int i = 0;
+		// set index to first non-equal ordinal or length of shortest version string
+		while (i < vals1.length && i < vals2.length && vals1[i].equals(vals2[i]))
+			i++;
+		// compare first non-equal ordinal number
+		if (i < vals1.length && i < vals2.length){
+			int diff = Integer.valueOf(vals1[i]).compareTo(Integer.valueOf(vals2[i]));
+			return Integer.signum(diff);
+		}
+		// the strings are equal or one string is a substring of the other
+		// e.g. "1.2.3" = "1.2.3" or "1.2.3" < "1.2.3.4"
+		else
+			return Integer.signum(vals1.length - vals2.length);
 	}
 }
