ad6ed471fe2fe5bdb18f10526807ff5911290c2b
kernitus <2789734+kernitus@users.noreply.github.com>
2017-03-01 19:55:45 +0100

    Implemented bstats

--- src/main/java/gvlfm78/plugin/Hotels/HotelsMain.java
+++ src/main/java/gvlfm78/plugin/Hotels/HotelsMain.java
@@ -1,14 +1,16 @@
 package kernitus.plugin.Hotels;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.logging.Logger;
 
+import org.bstats.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import kernitus.plugin.Hotels.Metrics.Graph;
+import kernitus.plugin.Hotels.MCMetrics.Graph;
 import kernitus.plugin.Hotels.handlers.HotelsCommandHandler;
 import kernitus.plugin.Hotels.handlers.HotelsConfigHandler;
 import kernitus.plugin.Hotels.managers.Mes;
@@ -60,71 +62,73 @@ public class HotelsMain extends JavaPlugin{
 
 		//Logging to console the correct enabling of Hotels
 		getLogger().info(Mes.getStringNoPrefix("main.enable.success").replaceAll("%pluginname%", pdfFile.getName()).replaceAll("%version%", pdfFile.getVersion()));
-		//Metrics
+
+		final int hotelsCount = HotelsAPI.getHotelCount();
+
+		//MCStats Metrics
 		try {
-			Metrics metrics = new Metrics(this);
+			MCMetrics metrics = new MCMetrics(this);
 
 			Graph hotelAmount = metrics.createGraph("Amount of Hotels");
 			Graph language = metrics.createGraph("Language");
-			int count = HotelsAPI.getHotelCount();
 
 			//Hotel amount
-			switch(count) {
+			switch(hotelsCount) {
 			case 0:
-				hotelAmount.addPlotter(new Metrics.Plotter("1-3") {
+				hotelAmount.addPlotter(new MCMetrics.Plotter("0") {
 					@Override
 					public int getValue() {
 						return 0;
 					}
 				}); break;
 			case 1: case 2: case 3:
-				hotelAmount.addPlotter(new Metrics.Plotter("1-3") {
+				hotelAmount.addPlotter(new MCMetrics.Plotter("1-3") {
 					@Override
 					public int getValue() {
 						return 1;
 					}
 				}); break;
 			case 4: case 5:
-				hotelAmount.addPlotter(new Metrics.Plotter("4-5") {
+				hotelAmount.addPlotter(new MCMetrics.Plotter("4-5") {
 					@Override
 					public int getValue() {
 						return 2;
 					}
 				}); break;
 			case 6: case 7: case 8: case 9: case 10:
-				hotelAmount.addPlotter(new Metrics.Plotter("6-10") {
+				hotelAmount.addPlotter(new MCMetrics.Plotter("6-10") {
 					@Override
 					public int getValue() {
 						return 3;
 					}
 				}); break;
 			case 11: case 12: case 13: case 14: case 15:
-				hotelAmount.addPlotter(new Metrics.Plotter("11-15") {
+				hotelAmount.addPlotter(new MCMetrics.Plotter("11-15") {
 					@Override
 					public int getValue() {
 						return 4;
 					}
 				});
 			case 16: case 17: case 18: case 19: case 20:
-				hotelAmount.addPlotter(new Metrics.Plotter("16-20") {
+				hotelAmount.addPlotter(new MCMetrics.Plotter("16-20") {
 					@Override
 					public int getValue() {
 						return 5;
 					}
 				}); break;
 			default:
-				hotelAmount.addPlotter(new Metrics.Plotter(">20") {
+				hotelAmount.addPlotter(new MCMetrics.Plotter(">20") {
 					@Override
 					public int getValue() {
 						return 6;
 					}
-				}); break;
+				});
 			}
 
 			//Languages
 			switch(HotelsConfigHandler.getLanguage()){
 			case "en": case "enGB":
-				language.addPlotter(new Metrics.Plotter("English") {
+				language.addPlotter(new MCMetrics.Plotter("English") {
 					@Override
 					public int getValue() {
 						return 1;
@@ -132,51 +136,87 @@ public class HotelsMain extends JavaPlugin{
 				}); break;
 
 			case "it": case "itIT":
-				language.addPlotter(new Metrics.Plotter("Italian") {
+				language.addPlotter(new MCMetrics.Plotter("Italian") {
 					@Override
 					public int getValue() {
 						return 2;
 					}
 				}); break;
 			case "fr": case "frFR":
-				language.addPlotter(new Metrics.Plotter("French") {
+				language.addPlotter(new MCMetrics.Plotter("French") {
 					@Override
 					public int getValue() {
 						return 3;
 					}
 				}); break;
 			case "ru": case "ruRU":
-				language.addPlotter(new Metrics.Plotter("Russian") {
+				language.addPlotter(new MCMetrics.Plotter("Russian") {
 					@Override
 					public int getValue() {
 						return 4;
 					}
 				}); break;
 			case "zhCN":
-				language.addPlotter(new Metrics.Plotter("Simplified Chinese") {
+				language.addPlotter(new MCMetrics.Plotter("Simplified Chinese") {
 					@Override
 					public int getValue() {
 						return 5;
 					}
 				}); break;
 			case "zhTW":
-				language.addPlotter(new Metrics.Plotter("Traditional Cninese") {
+				language.addPlotter(new MCMetrics.Plotter("Traditional Cninese") {
 					@Override
 					public int getValue() {
 						return 6;
 					}
 				}); break;
 			default:
-				language.addPlotter(new Metrics.Plotter("Custom") {
+				language.addPlotter(new MCMetrics.Plotter("Custom") {
 					@Override
 					public int getValue() {
 						return 7;
 					}
-				}); break;
+				});
 			}
 
 			metrics.start();
 		} catch (IOException e) { /*Failed to submit stats */ }
+
+		//BStats Metrics
+		Metrics metrics = new Metrics(this);
+
+		//Hotel amount
+		metrics.addCustomChart(new Metrics.SimpleBarChart("hotel_amount") {
+			@Override
+			public HashMap<String, Integer> getValues(HashMap<String, Integer> values) {
+				switch(hotelsCount) {
+				case 0: values.put("0", 1); break;
+				case 1: case 2: case 3:	values.put("1-3", 1); break;
+				case 4: case 5: values.put("4-5", 1); break;
+				case 6: case 7: case 8: case 9: case 10: values.put("6-10", 1); break;
+				case 11: case 12: case 13: case 14: case 15: values.put("11-15", 1); break;
+				case 16: case 17: case 18: case 19: case 20: values.put("16-20", 1); break;
+				default: values.put(">20", 1);
+				}				
+				return values;
+			}
+		});
+
+		//Locale
+		metrics.addCustomChart(new Metrics.SimplePie("locale_language") {
+			@Override
+			public String getValue() {
+				switch(HotelsConfigHandler.getLanguage()){
+				case "en": case "enGB": return "English";
+				case "it": case "itIT": return "Italian";
+				case "fr": case "frFR": return "French";
+				case "ru": case "ruRU": return "Russian";
+				case "zhCN": return "Simplified Chinese";
+				case "zhTW": return "Traditional Chinese";
+				default: return "Custom";
+				}
+			}
+		});
 
 		//Checking for updates
 		if(getConfig().getBoolean("checkForUpdates")){
